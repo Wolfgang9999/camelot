5c752531bb948113fe0376d453e60a59285222d2
vijuc895 <vijender.chaudhary@indwealth.in>
2021-02-25 03:16:13 +0530

    num column support

--- camelot/__main__.py
+++ camelot/__main__.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+import traceback
+from tabulate import tabulate
+
 __all__ = ("main",)
 
 
-def main():
-    from camelot.cli import cli
+def pprint(df, return_str=False):
+    try:
+        pretty = tabulate(df, headers='keys', tablefmt='psql')
+        if return_str:
+            return pretty
+        else:
+            print(pretty)
+    except Exception as e:
+        print(traceback.format_exc())
 
-    cli()
+
+def main():
+    from camelot.io import read_pdf
+    tables = read_pdf(
+        "/Users/vijender/indvision-data/2020_11_0319_33_38_008446_1604432018_unlckd_reptd_redacted_merged.pdf",
+        flavor='stream', table_areas=["0,1910,595,1860"])
+    tables.export('foo.csv', f='csv', compress=False)
+    data = tables[0].df
+    pprint(data)
 
 
 if __name__ == "__main__":
